4bef2f25486b91df8205cb125911f028fd9bc383
Diaxa <pandiaxa@gmail.com>
2022-03-26 12:01:54 +0200

    Added photo from static

--- src/main/java/com/example/Springonlinestorenew/uploads/FileUploadController.java
+++ src/main/java/com/example/Springonlinestorenew/uploads/FileUploadController.java
@@ -4,22 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
+import com.example.Springonlinestorenew.entity.Product;
+import com.example.Springonlinestorenew.service.ProductService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
@@ -27,9 +19,12 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class FileUploadController {
     private final  StorageService storageService;
 
+    private final ProductService productService;
 
-    public FileUploadController(StorageService storageService) {
+
+    public FileUploadController(StorageService storageService, ProductService productService) {
         this.storageService = storageService;
+        this.productService = productService;
     }
 
     private static String UPLOADED_FOLDER = "src/main/resources/static/images/"; //saglabās šajā vietā ja nav
@@ -56,7 +51,11 @@ public class FileUploadController {
             Files.write(path, bytes);
 
             redirectAttributes.addFlashAttribute("message",
-                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+                    "You successfully uploaded '" + file.getOriginalFilename() + "' "+ path);
+
+            Product product = productService.getProductById(16L);
+            product.setImage("images/" + file.getOriginalFilename());
+            productService.saveProduct(product);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -65,9 +64,11 @@ public class FileUploadController {
         return "redirect:/uploadStatus";
     }
 
+
     @GetMapping("/uploadStatus")
     public String uploadStatus() {
-        return "uploadStatus";
+
+        return "redirect:/products";
     }
 
 
